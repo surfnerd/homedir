5332c3045745c02038250a7e1d36c204cf69810c
Christopher Goy <goyenator@gmail.com>
2016-10-04 22:48:49 -0700

    add my tmux configuration

--- backupandreplace.sh
+++ backupandreplace.sh
@@ -4,7 +4,9 @@ cd ~
 mv ~/.vim .vim-old
 mv ~/.vimrc .vimrc-old
 mv ~/.bash_profile .bash_profile-old
+mv ~/.tmux.conf .tmux.conf-old
 
 ln -s homedir/.vim ~/.vim
 ln -s homedir/.vimrc ~/.vimrc
 ln -s homedir/.bash_profile ~/.bash_profile
+ln -s homedir/.tmux.conf ~/.tmux.conf
