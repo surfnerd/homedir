cec14d1d1bfe29d80367bed6ae29d12ef25bb3e8
Christopher Goy <goyenator@gmail.com>
2016-10-16 12:07:58 -0700

    add git config.  shorten script name, add command arguments and usage

---
+++ homedir.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "$(tput bold)usage:$(tput sgr0)"
+	echo "	$(tput bold)update$(tput sgr0)"
+	echo "		- updates your configuration files in your home director and backs up the old ones with the suffix '-old'"
+	echo "	$(tput bold)clean$(tput sgr0)"
+	echo "		- cleans up the backed up files"
+	exit 1
+fi
+
+if [[ $1 == "update" ]]
+then
+	cd ~
+	mv ~/.vim ~/.vim-old
+	mv ~/.vimrc ~/.vimrc-old
+	mv ~/.bash_profile ~/.bash_profile-old
+	mv ~/.tmux.conf ~/.tmux.conf-old
+	mv ~/.gitconfig ~/.gitconfig-old
+	ln -s ~/homedir/.vim ~/.vim
+	ln -s ~/homedir/.vimrc ~/.vimrc
+	ln -s ~/homedir/.bash_profile ~/.bash_profile
+	ln -s ~/homedir/.tmux.conf ~/.tmux.conf
+	ln -s ~/homedir/.gitconfig ~/.gitconfig
+	echo "updated your configurations"
+
+elif [[ $1 == "clean" ]]
+then
+	echo "Cleaning up backup files:" 
+	cd ~
+	find . -maxdepth 1 -regex '.*\-old$' -exec rm {} + -print
+	echo "Finished"
+fi
+
+
